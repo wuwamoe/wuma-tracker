94222bd2706ee2e630f73de079624fc8f2581802
HyeongSeok Ryu <devingryu@korea.ac.kr>
2025-06-14 14:04:04 +0900

    fix: 2.4 second gworld

--- src-tauri/src/external.rs
+++ src-tauri/src/external.rs
@@ -34,7 +34,7 @@ pub struct WinProc {
 
 impl WinProc {
     const OFFSET: WuwaOffset = WuwaOffset {
-        global_gworld: 0x84C35C0,
+        global_gworld: 0x84C55C0,
         uworld_persistentlevel: 0x38,
         uworld_owninggameinstance: 0x1C0,
         ulevel_lastworldorigin: 0xC8,
